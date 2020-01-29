a4e986d8affaf5b941a6e28b46257d507021814a
Quan Vu <46224928+quan-vu@users.noreply.github.com>
2020-01-29 21:46:55 +0700

    Config Heroku: use free postgres cloud

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,8 @@
 # instance/config.py
 
 SECRET_KEY = '#%^^p9Bv9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://flaskcrud_admin:flaskcrud@localhost/flaskcrud_db'
+# Use local database
+# SQLALCHEMY_DATABASE_URI = 'mysql://flaskcrud_admin:flaskcrud@localhost/flaskcrud_db'
+
+# Use free cloud Postgres for demo
+postgres://bdueqdfd:DLtJk8kF862Xs-9PkcGsllx0gE3IfSId@rajje.db.elephantsql.com:5432/bdueqdfd
