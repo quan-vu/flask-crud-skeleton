d18f02d8658f03cac601f48fa47354efda664026
Quan Vu <46224928+quan-vu@users.noreply.github.com>
2020-01-29 21:50:59 +0700

    Config Heroku: DATABASE_URI

--- instance/config.py
+++ instance/config.py
@@ -1,8 +1,9 @@
 # instance/config.py
+import os
 
 SECRET_KEY = '#%^^p9Bv9%$i01'
 # Use local database
 # SQLALCHEMY_DATABASE_URI = 'mysql://flaskcrud_admin:flaskcrud@localhost/flaskcrud_db'
 
 # Use free cloud Postgres for demo
-postgres://bdueqdfd:DLtJk8kF862Xs-9PkcGsllx0gE3IfSId@rajje.db.elephantsql.com:5432/bdueqdfd
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
